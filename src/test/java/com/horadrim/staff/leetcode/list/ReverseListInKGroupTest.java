490fdbd9a8d9d1798a5ebceb925f3c6fe8ac727a
Horadrim-TalRasha <524948250@qq.com>
2025-02-01 20:31:08 +0800

    add reversing by multiple turns case

--- src/test/java/com/horadrim/staff/leetcode/list/ReverseListInKGroupTest.java
+++ src/test/java/com/horadrim/staff/leetcode/list/ReverseListInKGroupTest.java
@@ -43,6 +43,16 @@ public class ReverseListInKGroupTest {
         Assertions.assertEquals(list.getNodeByIndex(3).getData(), 5);
     }
 
+    @Test
+    public void reverseByMultipleTurnsTest() {
+        ReverseListInKGroup testInstance = new ReverseListInKGroup();
+        SimpleList list = constructVElementsTestList();
+        testInstance.solution(list, 2);
+        Assertions.assertEquals(list.getNodeByIndex(1).getData(), 4);
+        Assertions.assertEquals(list.getNodeByIndex(2).getData(), 3);
+        Assertions.assertEquals(list.getNodeByIndex(3).getData(), 6);
+    }
+
     private SimpleList constructSimpleTestList() {
         SimpleList list = new SimpleList();
         list.batchPush(list.new SimpleListNode(3))
@@ -58,6 +68,17 @@ public class ReverseListInKGroupTest {
             .batchPush(list.new SimpleListNode(4))
             .batchPush(list.new SimpleListNode(5))
             .batchPush(list.new SimpleListNode(6));
+
+        return list;
+    }
+
+    private SimpleList constructVElementsTestList() {
+        SimpleList list = new SimpleList();
+        list.batchPush(list.new SimpleListNode(3))
+            .batchPush(list.new SimpleListNode(4))
+            .batchPush(list.new SimpleListNode(5))
+            .batchPush(list.new SimpleListNode(6))
+            .batchPush(list.new SimpleListNode(7));
 
         return list;
     }
