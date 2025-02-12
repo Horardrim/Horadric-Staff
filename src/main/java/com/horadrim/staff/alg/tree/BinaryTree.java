38c72335892dc5c3b1dd25a8b9e179ed333880f5
Horadrim-TalRasha <524948250@qq.com>
2025-02-12 23:29:25 +0800

    implement BinaryTree isConnect function

--- src/main/java/com/horadrim/staff/alg/tree/BinaryTree.java
+++ src/main/java/com/horadrim/staff/alg/tree/BinaryTree.java
@@ -1,5 +1,9 @@
 package com.horadrim.staff.alg.tree;
 
+import java.util.Set;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class BinaryTree {
     private BinaryTreeNode root;
@@ -16,14 +20,6 @@ public class BinaryTree {
         return root;
     }
 
-    public void addRight(BinaryTreeNode child) {
-        
-    }
-
-    public boolean isBSTree() {
-        return isBSTree(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
-    }
-
     /*
      * 可以分别通过深度搜索和广度搜索以及判断入度来分析一颗二叉树是否有连通
      */
@@ -31,8 +27,37 @@ public class BinaryTree {
         return false;
     }
 
+    /*
+     * 是否有路径其所有节点的和等于target
+     */
     public boolean hasPathSum(int targetSum) {
         return hasPathSum(root, targetSum);
+    }
+
+    /*
+     * 两个节点的最低公共祖先节点
+     */
+    public BinaryTreeNode lowestCommonAncestor(BinaryTreeNode p, BinaryTreeNode q) {
+        return lowestCommonAncestor(root, p, q);
+    }
+
+    private BinaryTreeNode lowestCommonAncestor(BinaryTreeNode root, BinaryTreeNode p, BinaryTreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+
+        // 递归地在左子树和右子树中查找 p 和 q
+        BinaryTreeNode left = lowestCommonAncestor(root.left, p, q);
+        BinaryTreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        // 如果左子树和右子树的递归调用都返回了非空节点
+        if (left != null && right != null) {
+            // 当前节点就是最近公共祖先
+            return root;
+        }
+
+        // 如果只有一个子树返回了非空节点，返回该子树的返回值
+        return left != null ? left : right;
     }
 
     /*
@@ -50,6 +75,67 @@ public class BinaryTree {
         targetSum -= node.data;
 
         return hasPathSum(node.left, targetSum) || hasPathSum(node.right, targetSum);
+    }
+
+    /*
+     * 判断二叉树是否连通
+     */
+    public boolean isConnected() {
+        Set<BinaryTreeNode> visited = new HashSet<>();
+        return DFSConnected(root, visited);
+    }
+
+    /*
+     * 通过深度搜索判断二叉树连通性
+     */
+    private boolean DFSConnected(BinaryTreeNode node, Set<BinaryTreeNode> visited) {
+        if (node == null) {
+            return false;
+        }
+
+        if (visited.contains(node)) {
+            return true;
+        }
+
+        visited.add(node);
+        return DFSConnected(node.left, visited) || DFSConnected(node.right, visited);
+    }
+
+    /*
+     * 通过广度搜索判断二叉树连通性
+     */
+    public boolean BFSConnected() {
+        if (root == null) {
+            return false;
+        }
+
+        Queue<BinaryTreeNode> queue = new LinkedList<>();
+        Set<BinaryTreeNode> visited = new HashSet<>();
+        queue.offer(root);
+
+        while(!queue.isEmpty()) {
+            BinaryTreeNode node = queue.poll();
+            if (visited.contains(node)) {
+                return true;
+            }
+
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+
+        return false;
+    }
+
+    /*
+     * 判断该二叉树是否为二叉搜索树
+     */
+    public boolean isBSTree() {
+        return isBSTree(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
 
     private boolean isBSTree(BinaryTreeNode node, int lower, int upper) {
