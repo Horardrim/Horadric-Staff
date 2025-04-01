e821a345f77cc46a06dfba710f444907db812f38
Horadrim-TalRasha <524948250@qq.com>
2025-04-01 11:24:57 +0800

    add question description

---
+++ src/main/java/com/horadrim/staff/ltcd/array/ArrayPartition.java
@@ -0,0 +1,12 @@
+/*
+ * 给定一个整数X和一个数组arr，要求将数组分成三个部分，
+ * 所有小于X的元素放在第一部分，等于X的放在第二部分，大于
+ * X的放在第三部分，每个部分内的顺序可以是任意的，但需要
+ * 保证这三个部分位置的相对正确。
+ */
+
+package com.horadrim.staff.ltcd.array;
+
+public class ArrayPartition {
+    
+}
