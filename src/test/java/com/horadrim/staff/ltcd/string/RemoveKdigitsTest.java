90dac8ca6fcd82f897b525ae63f89d23972e6c08
Horadrim-TalRasha <524948250@qq.com>
2025-07-16 18:06:39 +0800

    fix build error

--- src/test/java/com/horadrim/staff/ltcd/string/RemoveKdigitsTest.java
+++ src/test/java/com/horadrim/staff/ltcd/string/RemoveKdigitsTest.java
@@ -1,5 +1,13 @@
 package com.horadrim.staff.ltcd.string;
 
+import org.junit.jupiter.api.Test;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class RemoveKdigitsTest {
-    feafeae
+    @Test
+    public void normalTest() {
+
+    }
 }
