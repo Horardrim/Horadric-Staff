0c32b4ffe2ddc056f01b73a02e1db41e7e4cacfd
Horadrim-TalRasha <524948250@qq.com>
2025-07-17 18:37:45 +0800

    implement remove k digital problem

--- src/main/java/com/horadrim/staff/ltcd/string/RemoveKdigits.java
+++ src/main/java/com/horadrim/staff/ltcd/string/RemoveKdigits.java
@@ -1,7 +1,11 @@
 package com.horadrim.staff.ltcd.string;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
+
 /*
- * 给你一个以字符串表示的非负整数 num 和一个整数 k ，移除这个数中的 k 位数字，使得剩下的数字最小。请你以字符串形式返回这个最小的数字。
+ * 给你一个以字符串表示的非负整数 num 和一个整数 k ，移除这个数中的 k 位(可以不连续)数字，使得剩下的数字最小。请你以字符串形式返回这个最小的数字。
  
  * 示例 1 ：
 
@@ -20,8 +24,36 @@ package com.horadrim.staff.ltcd.string;
  * 解释：从原数字移除所有的数字，剩余为空就是 0 。
  */
 
+ /*
+ * Tag: 单调栈
+ */
 public class RemoveKdigits {
-    int solution(String str, int k) {
-        return 0;
+    String solution(String num, int k) {
+                Deque<Character> deque = new LinkedList<Character>();
+        int length = num.length();
+        for (int i = 0; i < length; ++i) {
+            char digit = num.charAt(i);
+            while (!deque.isEmpty() && k > 0 && deque.peekLast() > digit) {
+                deque.pollLast();
+                k--;
+            }
+            deque.offerLast(digit);
+        }
+        
+        for (int i = 0; i < k; ++i) {
+            deque.pollLast();
+        }
+        
+        StringBuilder ret = new StringBuilder();
+        boolean leadingZero = true;
+        while (!deque.isEmpty()) {
+            char digit = deque.pollFirst();
+            if (leadingZero && digit == '0') {
+                continue;
+            }
+            leadingZero = false;
+            ret.append(digit);
+        }
+        return ret.length() == 0 ? "0" : ret.toString();
     }
 }
--- src/test/java/com/horadrim/staff/ltcd/string/RemoveKdigitsTest.java
+++ src/test/java/com/horadrim/staff/ltcd/string/RemoveKdigitsTest.java
@@ -1,5 +1,6 @@
 package com.horadrim.staff.ltcd.string;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import lombok.extern.slf4j.Slf4j;
@@ -8,6 +9,8 @@ import lombok.extern.slf4j.Slf4j;
 public class RemoveKdigitsTest {
     @Test
     public void normalTest() {
-
+        String num = new String("12345264");
+        String result = new RemoveKdigits().solution(num, 4);
+        Assertions.assertTrue(result.equals("1224"));
     }
 }
