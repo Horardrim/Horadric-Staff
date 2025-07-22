68aeff14cfb57fe5b697178559f7e56478ace1af
Horadrim-TalRasha <524948250@qq.com>
2025-07-22 16:33:35 +0800

    add test cases for is subsequence problem

--- src/test/java/com/horadrim/staff/ltcd/string/IsSubSequenceTest.java
+++ src/test/java/com/horadrim/staff/ltcd/string/IsSubSequenceTest.java
@@ -1,5 +1,6 @@
 package com.horadrim.staff.ltcd.string;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +11,23 @@ public class IsSubSequenceTest {
     void test() {
         IsSubSequence isSubSequence = new IsSubSequence();
         boolean solution = isSubSequence.solution("abc", "ahbgdc");
+        Assertions.assertTrue(solution);
+        log.info("solution: {}", solution);
+    }
+
+    @Test
+    void duplicate() {
+        IsSubSequence isSubSequence = new IsSubSequence();
+        boolean solution = isSubSequence.solution("adc", "ahbagdc");
+        Assertions.assertTrue(solution);
+        log.info("solution: {}", solution);
+    }
+
+    @Test
+    void negative() {
+        IsSubSequence isSubSequence = new IsSubSequence();
+        boolean solution = isSubSequence.solution("axc", "ahbagdc");
+        Assertions.assertFalse(solution);
         log.info("solution: {}", solution);
     }
 }
